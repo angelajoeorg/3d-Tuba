1a72cbc9ec09fd1f0730148e9ce9d3dcff39642f
Angela <a.joeorg@hotmail.com>
2019-09-04 16:21:36 +0300

    keskonnad

--- components/Ceiling.js
+++ components/Ceiling.js
@@ -1,7 +1,7 @@
 export default {
     template: `
     	<a-entity class="ceiling" >
-        	<a-plane rotation="90 0 0"  width="10" height="10" color="white"></a-plane>
+        	<a-plane rotation="90 0 0"  width="10" height="10" material="side: double; color:white; transparent:true; opacity:0.4"></a-plane>
             <slot />
         </a-entity>
     `
--- index.js
+++ index.js
@@ -16,18 +16,19 @@ new Vue({
           </a-assets>
         </template>
             <Ceiling position="0 4 0">
-        <a-entity material="color:white; emissive: #faf9ff; emissiveIntensity: 2" class="chandelier" position="0 -0.70 0" scale="0.001 0.001 0.001" obj-model="obj: #chandelier"></a-entity>
+        <a-entity material="color:rgba(255,255,255,0.3); emissive: #faf9ff; emissiveIntensity: 1" class="chandelier" position="0 -0.70 0" scale="0.001 0.001 0.001" obj-model="obj: #chandelier"></a-entity>
           <a-entity light="type: point; intensity: 0.6; color: #faf9ff; castShadow: true " position="0 -0.7 0" ></a-entity>
-          <a-entity light="type: ambient; intensity: 0.5; color: #faf9ff "></a-entity>  
+          <a-entity light="type: ambient; intensity: 0.1; color: #faf9ff "></a-entity>  
         </Ceiling>
-        <Wall position="0 0 -5"/>
-      	<Wall position="-5 0 0" rotation="0 90 0"/>
+        <!-- <Wall position="0 0 -5"/> -->
+      	<!-- <Wall position="-5 0 0" rotation="0 90 0"/> -->
       	<Wall position="5 0 0" rotation="0 -90 0"/>
         <Wall position="0 0 5" rotation="0 -180 0"/>
         <Box position="1 1 0" shadow="cast:true"/>
         <Floor/>
-        
+          <floor position="0 0.01 0"/>
         <Floor />
+        <a-entity environment="preset: volcano; fog:3; groundColor: #100; grid: cross"></a-entity>
       </Scene>
     </Layout>
     `,
