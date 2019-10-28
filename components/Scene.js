17390e08a555a75dccee5882eebd5e7a53ba726d
Angela <a.joeorg@hotmail.com>
2019-10-28 12:15:17 +0200

    nunnu

--- components/Scene.js
+++ components/Scene.js
@@ -1,9 +1,7 @@
 export default {
     template: `
         <a-scene>
-        <a-assets>
-      <a-asset-item id="chandelier" src="assets/models/luhter1.obj"></a-asset-item>
-       </a-assets>
+        <slot name="assets" />
           <a-entity position="0 0 -4">
             <slot /> 
             <a-sky color="#ECECEC"></a-sky>
