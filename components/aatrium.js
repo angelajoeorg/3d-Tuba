091f26a9d4c0233d8b8b5b38a8cd5de18bdb3c36
Angela <a.joeorg@hotmail.com>
2019-10-31 15:49:01 +0200

    nunnnu

---
+++ components/aatrium.js
@@ -0,0 +1,7 @@
+export default {
+    template: `
+    	<a-entity>
+        	<a-plane position="0 2 0" color="white" width="1.5" height="4" class="aatrium" material="side: double; transparent:true; opacity:0.9.5"></a-plane>
+        </a-entity>
+    `
+}
--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import Box from './components/Box.js'
 import Wall from './components/Wall.js'
 import Floor from './components/Floor.js'
 import Ceiling from './components/Ceiling.js'
+import aatrium from './components/aatrium.js'
 new Vue({
     el: '#app',
-    components: { Layout, Scene, Box, Wall, Floor, Ceiling },
+    components: { Layout, Scene, Box, Wall, Floor, Ceiling, aatrium },
     template: `
     <Layout>
       <Scene>
@@ -20,9 +21,9 @@ new Vue({
           </a-assets>
         </template>
 
-        <a-camera position="0 1.8 2" tilt-turn twoway-motion></a-camera>
+        <a-camera position="0 1.8 10" tilt-turn twoway-motion></a-camera>
            
-        <a-entity class="nunnu" obj-model="obj:#nunnu"></a-entity>
+        <a-entity class="nunnu" rotation="0 180 0" material="side: double; color:red" scale="0.2 0.2 0.2" position="1.05 2.8 0"  obj-model="obj:#nunnu"></a-entity>
 
         <Ceiling position="0 4 0">
       
@@ -31,7 +32,12 @@ new Vue({
           <a-entity light="type: ambient; intensity: 0.1; color: #faf9ff "></a-entity>  
         </Ceiling>
         <!-- <Wall position="0 0 -5"/> -->
-      	<!-- <Wall position="-5 0 0" rotation="0 90 0"/> -->
+        <!-- <Wall position="-5 0 0" rotation="0 90 0"/> -->
+        <aatrium position="0.9 0 -0.06" rotation="0 -90 0"/>
+        <aatrium position="0.005 0 -1.2" rotation="0 0 0"/>
+        <aatrium position="0.6 2 0" rotation="0 90 0"/>
+        <aatrium position="1.2 0 0.06" rotation="0 0 0"/>
+
       	<Wall position="5 0 0" rotation="0 -90 0"/>
         <a-plane 
               position="1 2 -5"
@@ -41,7 +47,7 @@ new Vue({
               event-set__enter="_event: mouseenter; _target: #plakatitekst; visible: true"
               event-set__leave="_event: mouseleave; _target: #plakatitekst; visible: false"
           ></a-plane>
-        <Wall position="0 0 5" rotation="0 -180 0"/>
+        <Wall position="-5 0 0.01" rotation="0 90 0"/>
         <a-plane 
               position="4 2 -5"
               scale="2 4 0"
@@ -50,11 +56,16 @@ new Vue({
               event-set__enter="_event: mouseenter; _target: #plakatitekst; visible: true"
               event-set__leave="_event: mouseleave; _target: #plakatitekst; visible: false"
           ></a-plane>
-        <Box position="1 1 0" shadow="cast:true"/>
+        <Box position="0 1 0" shadow="cast:true"/>
         <Floor/>
           <floor position="0 0.01 0"/>
         <Floor />
-        <a-entity environment="preset: volcano; groundColor: #100; grid: cross"></a-entity>
+        <a-entity environment="preset: default; groundTexture: walkernoise; groundColor: #3A7036; groundColor2:#7DCC78 ;
+         grid: cross; dressing:trees; dressingAmount:10; dressingColor:#3C7005; dressingScale:30; dressingUniformScale:false;
+         skyType:gradient; skyColor:#190000;
+         
+         
+         "></a-entity>
       </Scene>
     </Layout>
     `,
